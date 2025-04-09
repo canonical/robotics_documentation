049e357c4c18a5a80e4b066cfd5ccc28289f852f
Jeremie Deray <jeremie.deray@canonical.com>
2025-04-09 13:34:58 +0200

    Fix project

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ import datetime
 #
 # TODO: Update with the official name of your project or product
 
-project = "Documentation starter pack"
+project = "Robotics"
 author = "Canonical Ltd."
 
 
@@ -69,7 +69,7 @@ copyright = "%s CC-BY-SA, %s" % (datetime.date.today().year, author)
 # NOTE: The Open Graph Protocol (OGP) enhances page display in a social graph
 #       and is used by social media platforms; see https://ogp.me/
 
-ogp_site_url = "https://canonical-starter-pack.readthedocs-hosted.com/"
+ogp_site_url = "https://canonical-robotics.readthedocs-hosted.com/"
 
 
 # Preview name of the documentation website
@@ -105,7 +105,7 @@ html_context = {
     # TODO: If there's no such website,
     #       remove the {{ product_page }} link from the page header template
     #       (usually .sphinx/_templates/header.html; also, see README.rst).
-    "product_page": "documentation.ubuntu.com",
+    "product_page": "ubuntu.com/robotics",
     # Product tag image; the orange part of your logo, shown in the page header
     #
     # TODO: To add a tag image, uncomment and update as needed.
