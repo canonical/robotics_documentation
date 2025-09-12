82f1c45f876248ae71fcd9f6bbc77b9d530b52a3
flor-cabral <florencia.cabralberenfus@canonical.com>
2025-09-12 11:41:15 +0200

    add redirect for heardening wp

--- docs/conf.py
+++ docs/conf.py
@@ -238,6 +238,7 @@ redirects = {
     # The unique ESM tutorial was broken down into how-tos & explanations
     "tutorials/ubuntu-pro/ros-esm-intro/": "../../../explanations/security/what-is-ros-esm/",
     "explanations/security/securing-ros-robotic-platforms/": "../../../how-to-guides/security/hardening-your-robot/",
+    "explanations/security/hardening-your-robot/": "../../../how-to-guides/security/hardening-your-robot/",
 }
 
 
